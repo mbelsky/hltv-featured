fa80ba7e4fc9764460f60598312c96ac6e4d61bf
Max Belsky <public.belsky@gmail.com>
2019-11-12 21:47:25 +0300

    Store filter value on stop

--- src/backend/commands.js
+++ src/backend/commands.js
@@ -1,3 +1,5 @@
+const { setFilter } = require('hltv-firebase-helper/manageUsers')
+
 const help = (ctx) =>
   ctx.reply(
     `Help and start.
@@ -5,10 +7,11 @@ const help = (ctx) =>
 /stop – stop getting notifications`,
   )
 
-const stop = (ctx) => {
-  // update status
-  // remove db record
-  // return successful message
+const stop = async (ctx) => {
+  // sendChatAction
+
+  // TODO: Wrap in try catch
+  await setFilter({ chatId: ctx.chat.id, filter: Number.MAX_SAFE_INTEGER })
   ctx.reply('You may subscribe again with /start')
 }
 
--- src/backend/middlewares/utils.js
+++ src/backend/middlewares/utils.js
@@ -1,3 +1,9 @@
+/**
+ * @param {string} text
+ * @returns number
+ */
+const getFilterValue = (text) => (text.match(/⭐️/g) || []).length
+
 module.exports = {
-  getFilterValue: (text) => (text.match(/⭐️/g) || []).length,
+  getFilterValue,
 }
