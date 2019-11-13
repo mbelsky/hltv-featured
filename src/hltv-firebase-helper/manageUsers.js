fb6e98840cb5f00d5ce5cc460cc399936e850e36
Max Belsky <public.belsky@gmail.com>
2019-11-13 21:14:12 +0300

    Implement getActiveUsers

--- src/hltv-firebase-helper/manageUsers.js
+++ src/hltv-firebase-helper/manageUsers.js
@@ -1,11 +1,25 @@
 const { USERS_COLLECTION } = require('./consts')
 const db = require('./firestore')
 
+async function getActiveUsers() {
+  const collectionSnapshot = await db
+    .collection(USERS_COLLECTION)
+    .where('filter', '<', Number.MAX_SAFE_INTEGER)
+    .get()
+
+  return collectionSnapshot.docs.reduce((users, documentSnapshot) => {
+    const data = documentSnapshot.data()
+
+    return data ? { ...users, [documentSnapshot.id]: data } : users
+  }, {})
+}
+
 async function setFilter({ chatId, filter }) {
   const userRef = db.collection(USERS_COLLECTION).doc(String(chatId))
   await userRef.set({ filter }, { merge: true })
 }
 
 module.exports = {
+  getActiveUsers,
   setFilter,
 }
