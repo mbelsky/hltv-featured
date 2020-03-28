a87868b71ab18901dcecde53ed161a5242db78af
Max Belsky <public.belsky@gmail.com>
2020-03-28 15:12:08 +0300

    Update scraper test

--- src/scraper/__tests__/scraper.spec.js
+++ src/scraper/__tests__/scraper.spec.js
@@ -1,16 +1,23 @@
 jest.mock('axios', () => () =>
   Promise.resolve({ data: require('./matches-example') }),
 )
+
 jest.mock('alerter', () => ({}))
+
 jest.mock('common/manageMatches', () => ({
   removeOutdatedMatches: () => Promise.resolve(),
-  saveFeaturedMatches: () => Promise.resolve(),
+  saveFeaturedMatches: jest.fn(() => Promise.resolve()),
 }))
 
 const scrap = require('../index')
 
 describe('scraper', () => {
   test('integration', async () => {
+    const manageMatches = require('common/manageMatches')
+    const { saveFeaturedMatches } = manageMatches
+
     expect(await scrap()).toBeUndefined()
+    expect(saveFeaturedMatches.mock.calls.length).toBe(1)
+    expect(saveFeaturedMatches.mock.calls[0]).toMatchSnapshot()
   })
 })
