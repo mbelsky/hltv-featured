03d643e673328df0381d1d6396f90b1a9823a7d1
Max Belsky <public.belsky@gmail.com>
2020-04-11 15:07:32 +0300

    Implement splitMatchesByFilter

---
+++ src/notifier/__tests__/utils.spec.js
@@ -0,0 +1,43 @@
+const { splitMatchesByFilter } = require('../utils')
+
+describe('splitMatchesByFilter', () => {
+  test('ASC list', () => {
+    const matches = [
+      { stars: 0, name: '#0' },
+      { stars: 1, name: '#1' },
+      { stars: 2, name: '#2' },
+      { stars: 3, name: '#3' },
+      { stars: 4, name: '#4' },
+      { stars: 5, name: '#5' },
+    ]
+
+    const result = splitMatchesByFilter(matches)
+
+    expect(result.length).toBe(6)
+    expect(result[0].length).toBe(matches.length)
+    expect(result[5].length).toBe(1)
+  })
+
+  test('Leaky list', () => {
+    const matches = [
+      { stars: 5, name: '#0' },
+      { stars: 5, name: '#1' },
+      { stars: 5, name: '#2' },
+      { stars: 1, name: '#5' },
+      { stars: 1, name: '#6' },
+    ]
+
+    const result = splitMatchesByFilter(matches)
+
+    expect(result.length).toBe(6)
+    expect(result[2].length).toBe(3)
+  })
+
+  test('Empty list', () => {
+    const matches = []
+
+    const result = splitMatchesByFilter(matches)
+
+    expect(result.length).toBe(0)
+  })
+})
--- src/notifier/utils.js
+++ src/notifier/utils.js
@@ -1,4 +1,25 @@
 const { convertToMessage } = require('common/formatMatches')
+
+function splitMatchesByFilter(matches = []) {
+  const result = []
+
+  let i = 0
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const filteredMatches = matches.filter(({ stars }) => stars >= i)
+
+    if (0 === filteredMatches.length) {
+      break
+    }
+
+    result[i] = filteredMatches
+    i++
+  }
+
+  return result
+}
+
 function getMatchesFeed(matches) {
   const feed = [[], [], [], [], [], []]
   const feedCount = feed.length
@@ -21,4 +42,5 @@ function getMatchesFeed(matches) {
 
 module.exports = {
   getMatchesFeed,
+  splitMatchesByFilter,
 }
