47a7cf45a121add84b296e45378709438abeabdc
Max Belsky <public.belsky@gmail.com>
2020-04-11 21:45:39 +0300

    Implement getTimeZoneOffsetsMap

--- src/notifier/utils.js
+++ src/notifier/utils.js
@@ -1,4 +1,48 @@
+const alerter = require('alerter')
 const { convertToMessage } = require('common/formatMatches')
+const { getTimezone } = require('common/getTimezone')
+
+async function getTimeZoneOffsetsMap(users) {
+  const timestamp = Date.now() / 1000
+  const userTimeZonesMap = Object.values(users)
+    .filter(({ location = {} }) => !!location.timeZoneId)
+    .reduce((map, { location }) => {
+      map[location.timeZoneId] = location
+
+      return map
+    }, {})
+  const getTimeZonePromises = Object.values(
+    userTimeZonesMap,
+  ).map(({ latitude, longitude }) =>
+    getTimezone({ latitude, longitude, timestamp }),
+  )
+
+  try {
+    // TODO: replace with allSettled
+    const timeZoneResponses = await Promise.all(getTimeZonePromises)
+    const result = timeZoneResponses
+      .filter(({ timeZoneId }) => !!timeZoneId)
+      .reduce((map, { dstOffset, rawOffset, timeZoneId }) => {
+        map[timeZoneId] = dstOffset + rawOffset
+
+        return map
+      }, {})
+
+    if (Object.keys(result).length !== getTimeZonePromises) {
+      alerter.warn(
+        new Error(
+          'There are invalid getTimeZone responses:\n' +
+            JSON.stringify(timeZoneResponses, null, 2),
+        ),
+      )
+    }
+
+    return result
+  } catch (e) {
+    alerter.error(e)
+    return {}
+  }
+}
 
 function splitMatchesByFilter(matches = []) {
   const result = []
@@ -41,6 +85,6 @@ function getMatchesFeed(matches) {
 }
 
 module.exports = {
-  getMatchesFeed,
+  getTimeZoneOffsetsMap,
   splitMatchesByFilter,
 }
