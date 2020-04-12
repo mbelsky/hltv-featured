bb2b6231d180885bfb57b1334126f7ff1581ed48
Max Belsky <public.belsky@gmail.com>
2020-04-12 15:49:45 +0300

    Support user timezones in morning feed

--- src/notifier/notifier.js
+++ src/notifier/notifier.js
@@ -9,9 +9,14 @@ const TEST_ACCS = (process.env.TEST_ACCS || '').split(',')
 const alerter = require('alerter')
 
 const Telegram = require('telegraf/telegram')
+const {
+  DEFAULT_TIMEZONE_OFFSET,
+  convertMatchesToFeed,
+  convertUnixTimestampToDateTime,
+} = require('common/formatMatches')
 const { getUpcomingMatches } = require('common/manageMatches')
 const { getActiveUsers, updateUser } = require('common/manageUsers')
-const { getMatchesFeed } = require('./utils')
+const { getTimeZoneOffsetsMap, splitMatchesByFilter } = require('./utils')
 
 async function notify() {
   const users = await getActiveUsers()
@@ -28,44 +33,90 @@ async function notify() {
     return
   }
 
-  const feed = getMatchesFeed(matches)
+  const timeZoneOffsetsMap = await getTimeZoneOffsetsMap(users)
+
+  const rawFeedList = splitMatchesByFilter(matches)
   const telegram = new Telegram(process.env.BOT_TOKEN)
 
-  Object.entries(users).forEach(([id, { filter, seenEmptyMessage }]) => {
-    if (process.env.NODE_ENV !== 'production' && !TEST_ACCS.includes(id)) {
-      return
-    }
-
-    let message = feed[filter]
-    let disable_notification = false
-
-    if (!message) {
-      if (seenEmptyMessage) {
+  Object.entries(users).forEach(
+    ([
+      id,
+      { filter, location = {}, seenEmptyMessage, seenCustomizeLocationMessage },
+    ]) => {
+      if (process.env.NODE_ENV !== 'production' && !TEST_ACCS.includes(id)) {
         return
       }
 
-      message = `There are no ${filter}-stars matches today. And I won't spam you with this message every morning.\n\nNext time just send /upcoming if no morning notification.`
-      disable_notification = true
-      seenEmptyMessage = true
-    }
+      const timeZoneOffset =
+        timeZoneOffsetsMap[location.timeZoneId] || DEFAULT_TIMEZONE_OFFSET
 
-    telegram
-      .sendMessage(Number(id), message, {
-        disable_notification,
-        disable_web_page_preview: true,
-        parse_mode: 'HTML',
-      })
-      .then(() => {
+      const userMatches = rawFeedList[filter].map(
+        ({ unixTimestamp, ...rest }) => {
+          const datetime = convertUnixTimestampToDateTime(
+            unixTimestamp,
+            timeZoneOffset * 1000,
+          )
+
+          return {
+            ...rest,
+            datetime,
+          }
+        },
+      )
+
+      const chatId = Number(id)
+      // eslint-disable-next-line no-unused-vars
+      const sendCustomizeLocationMessage = async () => {
+        const shouldSendMessage =
+          userMatches.length > 0 &&
+          undefined === location.timeZoneId &&
+          !seenCustomizeLocationMessage
+
+        if (!shouldSendMessage) {
+          return
+        }
+
+        const customizeLocationMessage =
+          'You may change matches timezone for your feed. Just tap on the 📎 attachment button, choose Location and send your city coordinates as a message.'
+
+        await telegram.sendMessage(chatId, customizeLocationMessage)
+        await updateUser(id, { seenCustomizeLocationMessage: true })
+      }
+
+      let message = convertMatchesToFeed(userMatches)
+      let disable_notification = false
+
+      if (!message) {
         if (seenEmptyMessage) {
-          return updateUser(id, { seenEmptyMessage: true })
+          return
         }
-      })
-      .catch((e = {}) => {
-        if (403 !== e.code) {
-          alerter.error('chat id: ' + id, e)
-        }
-      })
-  })
+
+        message = `There are no ${filter}-stars matches today. And I won't spam you with this message every morning.\n\nNext time just send /upcoming if no morning notification.`
+        disable_notification = true
+        seenEmptyMessage = true
+      }
+
+      telegram
+        .sendMessage(chatId, message, {
+          disable_notification,
+          disable_web_page_preview: true,
+          parse_mode: 'HTML',
+        })
+        .then(() => {
+          // TODO: check it doesn't update user if a message isn't empty and seenEmptyMessage is true
+          if (seenEmptyMessage) {
+            return updateUser(id, { seenEmptyMessage: true })
+          }
+        })
+        // Uncomment after alpha
+        // .then(sendCustomizeLocationMessage)
+        .catch((e = {}) => {
+          if (403 !== e.code) {
+            alerter.error('chat id: ' + id, e)
+          }
+        })
+    },
+  )
 }
 
 if (require.main === module) {
