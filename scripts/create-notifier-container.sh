641a8e417347fa29ec10d5fa828ab0e723c28475
Max Belsky <public.belsky@gmail.com>
2022-04-18 17:35:00 +0200

    Update bootstrap scripts

--- scripts/create-notifier-container.sh
+++ scripts/create-notifier-container.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-docker run \
-  --name=hltv-featured-scraper \
+docker create \
+  --name=hltv-featured-notifier \
   --env-file /???/.env \
   --env FIREBASE_CONFIG=/data/cert.json \
   --mount type=bind,src=/???/cert.json,dst=/data/cert.json,ro \
   --detach \
   mbelsky/hltv-featured:v??? \
-  node /app/src/scraper/scraper.js
+  node /app/src/notifier/notifier.js
--- scripts/create-scraper-container.sh
+++ scripts/create-scraper-container.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-docker run \
-  --name=hltv-featured-notifier \
+docker create \
+  --name=hltv-featured-scraper \
   --env-file /???/.env \
   --env FIREBASE_CONFIG=/data/cert.json \
   --mount type=bind,src=/???/cert.json,dst=/data/cert.json,ro \
   --detach \
   mbelsky/hltv-featured:v??? \
-  node /app/src/notifier/notifier.js
+  node /app/src/scraper/scraper.js
