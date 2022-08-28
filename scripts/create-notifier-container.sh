de629a0015d878e79f7bffdae07e45918294a523
Max Belsky <public.belsky@gmail.com>
2022-08-28 22:05:55 +0200

    Remove invalid flags from scripts

--- scripts/create-notifier-container.sh
+++ scripts/create-notifier-container.sh
@@ -5,6 +5,5 @@ docker create \
   --env-file /???/.env \
   --env FIREBASE_CONFIG=/data/cert.json \
   --mount type=bind,src=/???/cert.json,dst=/data/cert.json,ro \
-  --detach \
   mbelsky/hltv-featured:v??? \
   node /app/src/notifier/notifier.js
