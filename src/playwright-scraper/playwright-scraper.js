c9c24a215ed00f551fa98c270eabb2cf98eb933a
Max Belsky <public.belsky@gmail.com>
2023-03-15 22:08:10 +0100

    Implement playwright-scraper

--- src/playwright-scraper/playwright-scraper.js
+++ src/playwright-scraper/playwright-scraper.js
@@ -1,75 +1,69 @@
 const alerter = require('@hltvf/monitoring/alerter')
+const log = require('@hltvf/monitoring/logger').logFabric(
+  'requests-playwright-scraper',
+)
+const htmlToMatches = require('@hltvf/scraper/parseHtml')
 
-const playwright = require('playwright-chromium')
+const { getTeams } = require('common/getTeams')
+const {
+  removeOutdatedMatches,
+  saveFeaturedMatches,
+} = require('common/manageMatches')
+const { addTeams } = require('common/manageTeams')
+const { getHtml } = require('./get-html')
 
-async function launchChromium() {
-  return await playwright.chromium.launch()
+const root = process.env.ROOT_URL || 'https://www.hltv.org'
+const url = process.env.PAGE_URL || 'https://www.hltv.org/matches'
+
+const logResult = (data, matches) => {
+  if (!matches.length) {
+    alerter.warn('Zero matches scraped. HTML:\n\n' + data)
+  }
+
+  const message = `Scraped & saved ${matches.length} matches`
+  console.log(new Date().toUTCString(), message)
+
+  if ('production' === process.env.NODE_ENV) {
+    log({
+      level: 'info',
+      data: {
+        message,
+      },
+    })
+  }
 }
 
-async function scrap(browser, url) {
-  const context = await browser.newContext({
-    // acceptDownloads: false,
-    // javaScriptEnabled: false,
-    locale: 'en-GB',
+function scrap() {
+  log({
+    level: 'info',
+    data: {
+      message: 'Playwright-scraper has started',
+    },
   })
 
-  const page = await context.newPage()
+  return removeOutdatedMatches()
+    .catch(alerter.error)
+    .then(() => getHtml(url))
+    .then(
+      (data) => {
+        const matches = htmlToMatches(data, { root })
+        const teams = getTeams({ alerter, matches })
 
-  // await page.goto(url, {
-  //   waitUntil: 'domcontentloaded',
-  //   // waitUntil: 'load',
-  // })
-  // await page.goto(url)
+        const addTeamsPromise = addTeams(teams).catch(alerter.error)
+        const saveMatchesPromise = saveFeaturedMatches(matches)
+          .then(() => logResult(data, matches))
+          .catch(alerter.error)
 
-  try {
-    const requestPromise = page.waitForRequest(url, {
-      timeout: 5000,
-    })
-    const responsePromise = page.waitForResponse(url, {
-      timeout: 5000,
-    })
-
-    const [req, res] = await Promise.all([
-      requestPromise,
-      responsePromise,
-      page.goto(url),
-    ])
-    const resH = await res.allHeaders()
-    const reqH = await req.allHeaders()
-    const text = await res.text()
-    console.log(text.substring(0, 1))
-    console.log(`reqH`, reqH)
-    console.log(`resH`, resH)
-  } catch (e) {
-    console.log('hru')
-  }
-  //   const content = await page.content()
-
-  //   throw new Error(
-  //     `Wait for wq list selector failed. Page content: ${content}`,
-  //   )
-  // }
-
-  const innerHtml = await page.content()
-  return innerHtml
+        return Promise.all([addTeamsPromise, saveMatchesPromise])
+      },
+      (error) => {
+        alerter.error('Failed to get html', error)
+      },
+    )
 }
 
-async function getHtml() {
-  const browser = await launchChromium()
-
-  try {
-    const result = await scrap(browser, 'https://www.hltv.org/matches')
-
-    return result
-  } finally {
-    try {
-      await browser.close()
-    } catch (e) {
-      alerter.error('Failed to close browser', e)
-    }
-  }
+if (require.main === module) {
+  scrap()
+} else {
+  module.exports = scrap
 }
-
-getHtml().then((value) => {
-  console.log(value)
-})
